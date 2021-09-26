e98b5fc6a89bc902e85919e86acc97f6ec469d9b
sunguoy <sunguoy1991@gmail.com>
2021-09-26 14:20:36 +0800

    第一章的代码上传

---
+++ chapter1/struct_student.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+struct student{
+    int no;
+    char name[20];
+    char sex;//M表示男，F表示女
+    struct student* next;
+};
+
+int main()
+{
+    struct student stu[5]={
+                            {110,"Mike",'M'},
+                            {220,"Mary",'F'},
+                            {330,"Jack",'M'},
+                            {331,"Linda",'F'},
+                            {337,"Lucy",'F'}
+                            };//stu[0], stu[1], stu[2], stu[3], stu[4]
+    printf("%u %u %u %u %u\n",&stu[0],&stu[1],&stu[2],&stu[3], &stu[4]);
+    printf("%d \n", sizeof(stu[0]));
+
+    return 0;
+}
